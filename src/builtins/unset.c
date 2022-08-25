00c850295ee4fde95c6a6f71de96efdb609d99ef
HHHati <basile_deleeuw@yahoo.be>
2022-08-25 11:49:26 +0200

    B 8.0 Unset builtin

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,16 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkoyamba <mkoyamba@student.s19.be>         +#+  +:+       +#+        */
+/*   By: Bade-lee <bade-lee@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 17:22:49 by Bade-lee          #+#    #+#             */
-/*   Updated: 2022/08/24 17:07:17 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2022/08/25 11:44:50 by Bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-int	builtin_unset(void)
+static int	valid_name(char *name)
 {
+	size_t	i;
+
+	i = 0;
+	if (!name)
+		return (0);
+	if (name[i] != '_' && !ft_isalpha(name[i])
+		return (0);
+	while (name[i])
+	{
+		if (name[i] != '_' && !ft_isalnum(name[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	ajust_var(char **env, size_t i)
+{
+	while (env[i])
+	{
+		env[i] = env[i + 1];
+		i++;
+	}
+}
+
+static void	delete_var(char *name, char **env)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(name);
+	while (env[i])
+	{
+		if (!(ft_strncmp(name, env[i], len)) && env[i][len] == '=')
+		{
+			free(env[i]);
+			ajust_var(env, i);
+			return ;
+		}
+		i++;
+	}
+}
+
+int	builtin_unset(t_minishell *minishell)
+{
+	size_t	i;
+	int		error;
+
+	i = 1;
+	if (!minishell->env[i])
+		return (0);
+	error = 0;
+	while (minishell->env[i])
+	{
+		if (!valid_name(minishell->env[i]))
+		{
+			ft_putstr_fd("minishell: unset: '", STDERR);
+			ft_putstr_fd(minishell->env[i], STDERR);
+			ft_putendl_fd("': not a valid identifier", STDERR);
+			error = 1;
+		}
+		else
+			delete_var(minishell->env[i]), minishell->env)
+		i++;
+	}
+	if (error)
+		return(1)
 	return (0);
 }
